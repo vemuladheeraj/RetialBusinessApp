1a8f01dac030a35f9a323211457684ca353f77f1
Dheeraj Vemula <52877699+vemuladheeraj@users.noreply.github.com>
2020-11-08 11:21:12 +0530

    Add project files.

---
+++ RetialBusinessApp/Controllers/RetailController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RetialBusinessApp.Models;
+
+namespace RetialBusinessApp.Controllers
+{
+    public class RetailController : Controller
+    {
+        // GET: Retail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Retail/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Retail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Retail/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Retail/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = new RetailModel
+            {
+                OperatedSince = "TwoToFiveYears",
+
+            };
+            return View(model);
+        }
+
+        // POST: Retail/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Retail/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Retail/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RetialBusinessApp/Models/RetailModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RetialBusinessApp.Models
+{
+    public class RetailModel
+    {
+        public string RetailerShopName { get; set; }
+        public string OperatedSince { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactNumber { get; set; }
+        public string RetailerArea { get; set; }
+        public string Pincode { get; set; }
+        public string FranchiseArea { get; set; }
+        public string RetailerCategory { get; set; }
+        public string Essential { get; set; }
+        public string FPV { get; set; }
+        public string Branded { get; set; }
+        public string MonthlyTurnover { get; set; }
+        public string CurrentSuppliers { get; set; }
+        public string CometitorMonthlyPurchase { get; set; }
+        public string OnboardingSSK { get; set; }
+        public string Comments { get; set; }
+        public string KYC { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+
+    public enum OperatedSince
+    {
+        LessThan6Months,
+        SixMonthsToTwoYears,
+        TwoToFiveYears,
+        FiveToTenYears,
+        MoreThanTenYears
+    }
+}
